bd4a2410f876afab503beea2c3567590c4645968
juexro <juexro@163.com>
2020-08-06 17:22:45 +0800

    docs: annotation-editor demo

--- site/src/routes/components.tsx
+++ site/src/routes/components.tsx
@@ -1,7 +1,8 @@
+import AnnotationEditor from 'docs/annotation-editor.mdx';
 import Button from 'docs/button.mdx';
 
 const components = [
-  { component: Button, path: '/components/button', exact: true },
+  { component: AnnotationEditor, path: '/components/annotation-editor', exact: true },  { component: Button, path: '/components/button', exact: true },
 ];
 
 export default components;
